0c04ae5b0e4ab8819b6919b101d2b467a0083192
Juan Carrano <j.carrano@fu-berlin.de>
2018-05-15 14:43:11 +0200

    pkg/lua: Support test module.
    Compile with -DLUA_DEBUG to enable the test module. Additionally
     -DDEBUG_OVERRIDE_SIZES changes some buffer sizes to make it more
    likely that the test catch a bug.

--- pkg/lua/Makefile.lua
+++ pkg/lua/Makefile.lua
@@ -1,11 +1,5 @@
 SRC := $(filter-out loadlib.c lua.c luac.c,$(wildcard *.c))
 
-# This builds using POSIX system calls and some extra libraries, and
-# removes a compiler warning that warns against using tmpnam().
-ifeq ($(BOARD),native)
-CFLAGS += -DLUA_USE_LINUX
-endif
-
 # This is very important because lua defaults to 64 bit integers
 CFLAGS += -DLUA_32BITS -fstack-usage -Wstack-usage=128 \
           -Wno-error=stack-usage=128 -fconserve-stack \
